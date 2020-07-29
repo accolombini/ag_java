a038401b65bd5878b6f7cedcb48f2e51ecc7672a
accolombini <accolombini@gmail.com>
2020-07-29 14:49:17 -0300

    Integrando com MySQL

--- src/main/java/Algoritmo/Executar.java
+++ src/main/java/Algoritmo/Executar.java
@@ -7,12 +7,13 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
-
-import javax.swing.table.DefaultTableCellRenderer;
+import org.jfree.ui.ApplicationFrame;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
 
 class Produto {
     private String nome;
@@ -355,35 +356,38 @@ class Grafico extends ApplicationFrame {
 }
 
 public class Executar {
-    public static void main(String[] args) {
+
+   public static void main(String[] args) throws ClassNotFoundException, SQLException {
         List<Produto> listaProdutos = new ArrayList<>();
-        listaProdutos.add(new Produto("Geladeira Dako", 0.751, 999.90));
-        listaProdutos.add(new Produto("Iphone 6", 0.000089, 2911.12));
-        listaProdutos.add(new Produto("TV 55'", 0.400, 4346.99));
-        listaProdutos.add(new Produto("TV 50'", 0.290, 3999.90));
-        listaProdutos.add(new Produto("TV 42'", 0.200, 2999.00));
-        listaProdutos.add(new Produto("Notebook Dell", 0.0350, 2499.90));
-        listaProdutos.add(new Produto("Ventilador Panasonic", 0.496, 199.90));
-        listaProdutos.add(new Produto("Microondas Eletrolux", 0.0424, 308.66));
-        listaProdutos.add(new Produto("Microondas LG", 0.0544, 429.90));
-        listaProdutos.add(new Produto("Microondas Panasonic", 0.319, 299.29));
-        listaProdutos.add(new Produto("Geladeira Brastemp", 0.635, 849.90));
-        listaProdutos.add(new Produto("Geladeira Consul", 0.870, 1199.89));
-        listaProdutos.add(new Produto("Notebook Lenovo", 0.498, 1999.90));
-        listaProdutos.add(new Produto("Notebook Asus", 0.527, 3999.00));
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/produtos" +
+                        "? useTimezone = true & serverTimezone = UTC", "angelo", "1234");
+
+        Statement consulta = con.createStatement();
+        ResultSet rs = consulta.executeQuery("select nome, valor, espaco, quantidade from produtos");
+        while (rs.next()) {
+            for (int i = 0; i < rs.getInt("quantidade"); i++) {
+                listaProdutos.add(new Produto(rs.getString("nome"),
+                        rs.getDouble("espaco"), rs.getDouble("valor")));
+            }
+        }
 
         List espacos = new ArrayList<>();
         List valores = new ArrayList<>();
         List nomes = new ArrayList<>();
+
         for (Produto produto: listaProdutos) {
             espacos.add(produto.getEspaco());
             valores.add(produto.getValor());
             nomes.add(produto.getNome());
         }
-        Double limite = 3.0;
+
+        Double limite = 10.0;
         int tamanhoPopulacao = 20;
         Double taxaMutacao = 0.01;
         int numeroGeracoes = 100;
+
         AlgoritmoGenetico ag = new AlgoritmoGenetico(tamanhoPopulacao);
         List resultado = ag.resolver(taxaMutacao, numeroGeracoes, espacos, valores, limite);
 
